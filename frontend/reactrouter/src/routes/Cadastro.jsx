221000da987ddb5958ef2de35a8a0ed81e69d293
sarah-campos <sarahstephanycampos@gmail.com>
2023-11-02 00:33:57 -0300

    botao voltar

--- frontend/reactrouter/src/routes/Cadastro.jsx
+++ frontend/reactrouter/src/routes/Cadastro.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Cadastro = () => {
+  //redirect
+  const navigate = useNavigate()
+
+  const handleVoltar = () => {
+    return navigate("/");
+  }
+    
   return (
     <div>
-        <h1>Cadastro</h1>
+        <h1>Crie seu cadastro</h1>
+        <h3>Já possui uma conta? Faça seu *login*</h3>
+        <button onClick={handleVoltar}>Voltar</button>
     </div>
   )
 }
