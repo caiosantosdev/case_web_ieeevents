be12881e386f30f0678efe228c0659bd7d5bdb20
sarah-campos <sarahstephanycampos@gmail.com>
2023-11-03 18:19:41 -0300

    paginas cadastro e login

--- frontend/reactrouter/src/routes/Cadastro.jsx
+++ frontend/reactrouter/src/routes/Cadastro.jsx
@@ -1,35 +1,82 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import{ Link } from "react-router-dom"
+import "../styles/cadastro.css"
+import logo from '../assets/imagens/logo.png';
+import{useState} from "react";
 
 
-import '../styles/cadastro.css'
 
 const Cadastro = () => {
-  //redirect
-  const navigate = useNavigate()
+  const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [name, setName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [text, setText] = useState("")
+    const [textEnd, setTextEnd] = useState("")
+    const [tel, setTel] = useState("")
+    return (
+        <div className="container1">
+            <div className="container-register">
+                <div className="wrap-register">
+            <form className="register-form">
+                <span className="register-form-tittle">
+                    <img src={logo} alt="IEEEvents" />
+                </span>
+                <span className="register-form-tittle">Crie seu cadastro</span>
+                <div className="wrap-input">
+                    <input className={name !== "" ? 'has-val input' : 'input'} type='name'
+                        value={name} onChange={e => setName(e.target.value)} />
+                    <span className="focus-input" data-placeholder="Nome"></span>
+                </div>
+                <div className="wrap-input">
+                    <input className={lastName !== "" ? 'has-val input' : 'input'} type='name'
+                        value={lastName} onChange={e => setLastName(e.target.value)} />
+                    <span className="focus-input" data-placeholder="Sobrenome"></span>
+                </div>
+                <div className="wrap-input">
+                    <input className={tel !== "" ? 'has-val input' : 'input'} type="tel"
+                        value={tel} onChange={e => setTel(e.target.value)}
+                        pattern="[0-9]{2} [0-9]{5}-[0-9]{4}" />
+                    <span className="focus-input" data-placeholder="Telefone"></span>
+                </div>
+                <div className="wrap-input">
+                    <input className={ text!== "" ? 'has-val input' : 'input'} type='text'
+                        value={text} onChange={e => setText(e.target.value)} 
+                        pattern="\d{3}\.?\d{3}\.?\d{3}\-?\d{2}" />
+                    <span className="focus-input" data-placeholder="Cpf"></span>
+                </div>
+                <div className="wrap-input">
+                    <input className={textEnd !== "" ? 'has-val input' : 'input'} type='text'
+                        value={textEnd} onChange={e => setTextEnd(e.target.value)} />
+                    <span className="focus-input" data-placeholder="Endereço"></span>
+                </div>
 
-  const handleVoltar = () => {
-    return navigate("/");
-  }
-    
-  return (
-    <>
-        
-        <body>
-            <section className="cadastro">
-                <div className='cadastro-texto'>
-                    <h1 className='cadastro-texto__titulo'>Crie seu cadastro</h1>
-                    <h2 className='cadastro-texto__mensagem'>Já possui uma conta? Faça seu *login*</h2>
+                <div className="wrap-input">
+                    <input className={email !== "" ? 'has-val input' : 'input'} type='email'
+                        value={email} onChange={e => setEmail(e.target.value)} />
+                    <span className="focus-input" data-placeholder="Email"></span>
                 </div>
-                <div className='cadastro-botoes'>
-                    <button onClick={handleVoltar} className='botao' id="voltar">Voltar</button>
-                    <button onClick={handleVoltar} className='botao' id="cadastre">Cadastre-se</button>
+                <div className='wrap-input'>
+                    <input className={password !== "" ? 'has-val input' : 'input'} type='password'
+                        value={password} onChange={e => setPassword(e.target.value)} />
+                    <span className='focus-input' data-placeholder="Password"></span>
                 </div>
-            </section>
-        </body>
-        
-    </>
-  )
+
+                <div className="container-register-form-btn">
+                    <button className='register-form-btn' type="submit">Cadastre-se</button>
+
+                    <button className='voltar-form-btn' ><Link className="txt1" to='/'>Voltar</Link>
+                    </button>
+                </div>
+
+            </form>
+
+
+  
+            </div>
+      </div>
+    </div>
+    )
+
 }
 
 export default Cadastro
--- frontend/reactrouter/src/routes/Login.jsx
+++ frontend/reactrouter/src/routes/Login.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { useState } from 'react';
+import '../styles/login.css'
+
+import logo from '../assets/imagens/logo.png';
 
 const Login = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
   return (
-    <div>
-        <h1>Login</h1>
+    <div className="container">
+      <div className="container-login">
+        <div className="wrap-login">
+          <form className="login-form">
+            
+
+            <span className="login-form-tittle">
+              <img src={logo} alt="IEEEvents" />
+            </span>
+            <div className="wrap-input">
+              <input className={email !== "" ? 'has-val input' : 'input'} type='email'
+                value={email} onChange={e => setEmail(e.target.value)} />
+              <span className="focus-input" data-placeholder="E-mail"></span>
+            </div>
+
+            <div className='wrap-input'>
+              <input className={password !== "" ? 'has-val input' : 'input'} type='password'
+                value={password} onChange={e => setPassword(e.target.value)} />
+              <span className='focus-input' data-placeholder="Senha"></span>
+            </div>
+
+            <div className="container-login-form-btn">
+              <button className='login-form-btn'>Entrar</button>
+            </div>
+
+            <div className='text-center'>
+              <span className='txt1'>Não tem uma conta? </span>
+
+              <Link id="txt2" to='/cadastro'>Cadastre-se</Link>
+            </div>
+
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
