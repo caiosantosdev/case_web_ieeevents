f5fc4da2d02b7acdbf79db62c6b78b87aa0d0128
sarah-campos <sarahstephanycampos@gmail.com>
2023-11-04 22:45:43 -0300

    css cadastro

--- frontend/reactrouter/src/routes/Cadastro.jsx
+++ frontend/reactrouter/src/routes/Cadastro.jsx
@@ -4,13 +4,12 @@ import logo from '../assets/imagens/logo.png';
 import{useState} from "react";
 
 
-
 const Cadastro = () => {
   const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [name, setName] = useState("")
     const [lastName, setLastName] = useState("")
-    const [text, setText] = useState("")
+    const [cpf, setCpf] = useState("")
     const [textEnd, setTextEnd] = useState("")
     const [tel, setTel] = useState("")
     return (
@@ -18,31 +17,38 @@ const Cadastro = () => {
             <div className="container-register">
                 <div className="wrap-register">
             <form className="register-form">
-                <span className="register-form-tittle">
-                    <img src={logo} alt="IEEEvents" />
-                </span>
-                <span className="register-form-tittle">Crie seu cadastro</span>
-                <div className="wrap-input">
-                    <input className={name !== "" ? 'has-val input' : 'input'} type='name'
-                        value={name} onChange={e => setName(e.target.value)} />
-                    <span className="focus-input" data-placeholder="Nome"></span>
+                
+                    <div className="container__titulo">
+                        <img className="logo" src={logo} alt="IEEEvents" />
+                        <p className="titulo-principal">Crie seu cadastro</p>
+                        <p>Já possui uma conta? Faça seu <Link to='/login'>login</Link></p>
+                    </div>
+                
+                <div className="container__nomesobre">
+                    <div className="wrap-input">
+                        <input className={name !== "" ? 'has-val input' : 'input'} type='name'
+                            value={name} onChange={e => setName(e.target.value)} />
+                        <span className="focus-input" data-placeholder="Nome"></span>
+                    </div>
+                    <div className="wrap-input">
+                        <input className={lastName !== "" ? 'has-val input' : 'input'} type='name'
+                            value={lastName} onChange={e => setLastName(e.target.value)} />
+                        <span className="focus-input" data-placeholder="Sobrenome"></span>
+                    </div>
                 </div>
-                <div className="wrap-input">
-                    <input className={lastName !== "" ? 'has-val input' : 'input'} type='name'
-                        value={lastName} onChange={e => setLastName(e.target.value)} />
-                    <span className="focus-input" data-placeholder="Sobrenome"></span>
-                </div>
-                <div className="wrap-input">
-                    <input className={tel !== "" ? 'has-val input' : 'input'} type="tel"
-                        value={tel} onChange={e => setTel(e.target.value)}
-                        pattern="[0-9]{2} [0-9]{5}-[0-9]{4}" />
-                    <span className="focus-input" data-placeholder="Telefone"></span>
-                </div>
-                <div className="wrap-input">
-                    <input className={ text!== "" ? 'has-val input' : 'input'} type='text'
-                        value={text} onChange={e => setText(e.target.value)} 
-                        pattern="\d{3}\.?\d{3}\.?\d{3}\-?\d{2}" />
-                    <span className="focus-input" data-placeholder="Cpf"></span>
+                <div className="container__telCPF">
+                    <div className="wrap-input">
+                        <input className={tel !== "" ? 'has-val input' : 'input'} type="tel"
+                            value={tel} onChange={e => setTel(e.target.value)}
+                            pattern="[0-9]{2} [0-9]{5}-[0-9]{4}" />
+                        <span className="focus-input" data-placeholder="Telefone"></span>
+                    </div>
+                    <div className="wrap-input">
+                        <input className={ cpf== "" ? 'has-val input' : 'input'} type='cpf'
+                            value={cpf} onChange={e => setCpf(e.target.value)}
+                             maxLength={14}/> 
+                        <span className="focus-input" data-placeholder="CPF"></span>
+                    </div>
                 </div>
                 <div className="wrap-input">
                     <input className={textEnd !== "" ? 'has-val input' : 'input'} type='text'
@@ -53,19 +59,20 @@ const Cadastro = () => {
                 <div className="wrap-input">
                     <input className={email !== "" ? 'has-val input' : 'input'} type='email'
                         value={email} onChange={e => setEmail(e.target.value)} />
-                    <span className="focus-input" data-placeholder="Email"></span>
+                    <span className="focus-input" data-placeholder="E-mail"></span>
                 </div>
                 <div className='wrap-input'>
                     <input className={password !== "" ? 'has-val input' : 'input'} type='password'
                         value={password} onChange={e => setPassword(e.target.value)} />
-                    <span className='focus-input' data-placeholder="Password"></span>
+                    <span className='focus-input' data-placeholder="Senha"></span>
                 </div>
 
                 <div className="container-register-form-btn">
-                    <button className='register-form-btn' type="submit">Cadastre-se</button>
-
-                    <button className='voltar-form-btn' ><Link className="txt1" to='/'>Voltar</Link>
+                <button id="voltar" className='voltar-form-btn' ><Link className="txt1" to='/'>Voltar</Link>
                     </button>
+                    <button id="cadastrar" className='register-form-btn' type="submit">Cadastre-se</button>
+
+                    
                 </div>
 
             </form>
