18f3dcf0ebd3c80fa8eadf620fcc48c5a712b1c0
Carlos <carlosifsm@gmail.com>
2023-11-16 11:15:10 -0300

    console log retirados

--- backend/src/middlewares/Authentication.js
+++ backend/src/middlewares/Authentication.js
@@ -2,13 +2,12 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 function checkToken(req, res , next) {
-    console.log("entrou na autenticacao");
+    
     const authHeader = req.headers['authorization'];
     if(!authHeader){
         return res.json({ "message" : "Não há token inserido"});
     }
     const [frontToken, token] = authHeader.split(" ");
-    // const token = authHeader && authHeader.split(" ")[1];
 
     if(frontToken != "Bearer") {
         return res.json({"message" : "Token mal inserido"})
@@ -18,13 +17,11 @@ function checkToken(req, res , next) {
         return res.json({"message" : "Usuario nao encontrado"});
 
     }
-    console.log("entrou no try");
+
     try{
         const secret = process.env.SECRET;
         
         const verificacao = jwt.verify(token, secret);
-        console.log(verificacao);
-        console.log("verificou token");
 
         next();
     }catch(error){
--- frontend/reactrouter/src/main.jsx
+++ frontend/reactrouter/src/main.jsx
@@ -7,7 +7,7 @@ import './index.css'
 import { createBrowserRouter, RouterProvider,} from "react-router-dom";
 
 import TelaEntrada from './routes/TelaEntrada.jsx';
-import TelaPrincipal from './routes/TelaPRincipal.jsx';
+import TelaPrincipal from './routes/TelaPrincipal.jsx';
 import Perfil from './routes/perfil.jsx';
 import PaginaDoEvento from './routes/PaginaDoEvento.jsx';
 import MeusEventos from './routes/Meuseventos.jsx';
